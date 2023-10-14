42075375625e43ebfb5e54b5d6bf0208d88a6af7
Aluminum13 <61571737+Aluminum13@users.noreply.github.com>
2023-10-14 13:19:40 +0800

    Update 1.3.15.py

--- 1.3/1.3.15.py
+++ 1.3/1.3.15.py
@@ -3,6 +3,12 @@ n = int(num[0])
 x = int(num[1])
 y = int(num[2])
 if y % x == 0:
-    print(n - y // x)
+    print(max(0, n - y // x))
 else:
-    print(n - y // x - 1)
+    print(max(0, n - y // x - 1))
+
+"""
+虽然超纲用了if，但是更可怕的是
+我居然想不到不用if怎么做（
+如果有人不用if做出来了请告诉我！
+"""
