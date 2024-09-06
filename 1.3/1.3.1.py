1dc49f057f70e10ab32524bd2c7705036280b4b1
Aluminum13 <61571737+Aluminum13@users.noreply.github.com>
2024-09-06 15:02:47 +0800

    Update 1.3.1.py

--- 1.3/1.3.1.py
+++ 1.3/1.3.1.py
@@ -2,7 +2,7 @@ num = input().split()
 print(int(num[0]) + int(num[1]))
 
 """
-这里介绍另一种写法，之后将不再赘述：
+这里介绍另一种写法，之后将不再赘述（下次使用可能会在1.5），当然喜欢的朋友们现在就可以用起来了：
 a, b = map(int, input().split())
 print(a + b)
 """
