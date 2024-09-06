9b7bc815ddec5c94a9cf422f6665d18e41795ccf
Aluminum13 <61571737+Aluminum13@users.noreply.github.com>
2024-09-06 15:31:49 +0800

    Update 1.5.19.py

--- 1.5/1.5.19.py
+++ 1.5/1.5.19.py
@@ -1,8 +1,15 @@
-n = input()
+n = int(input())
 time = 0
 for i in range(n):
-  x, y, num_people = input().split()
-  time += (x**2 + y**2) / 2500 + num_people * 1.5
-if (time / 1):
-  time += 1
+    num = input().split()
+    x = float(num[0])
+    y = float(num[1])
+    num_people = float(num[2])
+    time += 2 * (x**2 + y**2)**0.5 / 50 + num_people * 1.5
+if (time % 1):
+    time += 1
 print(int(time))
+
+"""
+你知道的，向上取整也可以用math.ceil
+"""
