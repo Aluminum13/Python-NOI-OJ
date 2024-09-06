066ad4a0a59324efea331c7420566deee0072fec
Aluminum13 <61571737+Aluminum13@users.noreply.github.com>
2024-09-06 14:29:50 +0800

    Update 1.3.16.py

--- 1.3/1.3.16.py
+++ 1.3/1.3.16.py
@@ -2,7 +2,7 @@ a = input().split()
 b = input().split()
 x = float(a[0]) - float(b[0])
 y = float(a[1]) - float(b[1])
-print(f"{(x ** 2 + y ** 2) ** 0.5:.3f}")
+print(f"{(x**2 + y**2)**0.5:.3f}")
 
 """
 你当然还可以用内置库math里面的math.sqrt()来实现开根号。math.pow()来实现幂运算，还有其他更多实现方式
