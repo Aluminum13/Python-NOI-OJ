6754e3887c3f3eedf3d80859b711b87fab531d2f
Aluminum13 <61571737+Aluminum13@users.noreply.github.com>
2024-09-10 17:49:45 +0800

    Update 1.5.21.py

--- 1.5/1.5.21.py
+++ 1.5/1.5.21.py
@@ -33,5 +33,44 @@ for _ in itertools.count():
 
 print("End")
 这个方案利用python自带的标准库itertools中的count函数自动生成一个无限的数字序列，实现无限长循环。
-之后再通过break命令在适当的时候跳出。
+再通过break在适当的时候跳出。
+"""
+
+"""
+2.
+n = int(input())
+iterations = [n]
+
+for i in iterations:
+    if i == 1:
+        break
+    if i % 2:
+        next_value = i * 3 + 1
+        print("%d*3+1=%d" % (i, next_value))
+    else:
+        next_value = i // 2
+        print("%d/2=%d" % (i, next_value))
+    iterations.append(next_value)
+
+print("End")
+这个方案和1.5.7的思路基本一致，只是换了一种写法。
+在循环过程中动态修改list来实现基于for的不定长循环。
+"""
+
+"""
+def collatz(n):
+    if n == 1:
+        print("End")
+        return
+    if n % 2:
+        print(f"{n}*3+1={n * 3 + 1}")
+        collatz(n * 3 + 1)
+    else:
+        print(f"{n}/2={n // 2}")
+        collatz(n // 2)
+
+n = int(input())
+collatz(n)
+这是相对比较进阶的做法，利用了递归来模拟不定长的循环迭代。
+我们之前也曾经介绍过递归的概念（在介绍快速幂算法的时候），这里也一样，定义了一个collatz函数，并在其中不停的调用自身来实现迭代。
 """
